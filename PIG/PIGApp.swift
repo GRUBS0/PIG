8274e6e1874d3a9302854771a0d50458f1c2d353
Joseph <jjphillipse4@gmail.com>
2025-10-22 10:47:29 -0500

    Initial Commit

---
+++ PIG/PIGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIGApp.swift
+//  PIG
+//
+//  Created by joseph phillips on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PIGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
